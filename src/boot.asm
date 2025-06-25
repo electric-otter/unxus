8debf938788acc94d1669c543ca8ca04bfb3933b
electric-otter made Unxus! <electric-otter-github@proton.me>
2025-06-25 17:45:17 -0400

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -6,27 +6,25 @@ start:
     mov al, '!'          
     int 0x10
 
-    ; Load sector 2 (LBA 1) from disk into 0x7e00
+    ; Load fs.asm (sector 2) into 0x7c00 (overwriting self)
     mov ah, 0x02         ; BIOS read sectors
-    mov al, 1            ; number of sectors
+    mov al, 1            ; number of sectors (adjust if fs.asm > 1 sector)
     mov ch, 0            ; cylinder
     mov cl, 2            ; sector (starts at 1)
     mov dh, 0            ; head
     mov dl, 0x80         ; first hard disk
-    mov bx, 0x7e00       ; ES:BX buffer (ES=0x0000)
+    mov bx, 0x7c00       ; ES:BX buffer (ES=0x0000:0x7c00)
     int 0x13             ; BIOS disk service
 
     jc disk_error        ; if carry set, error
 
-    jmp 0x0000:0x7e00    ; jump to loaded code
+    jmp 0x0000:0x7c00    ; jump to loaded fs.asm code
 
 disk_error:
     mov al, 'E'
     int 0x10
     jmp $
 
-hang:
-    jmp hang
-
 times 510-($-$$) db 0
+dw 0xAA55
 dw 0xAA55

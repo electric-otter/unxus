be69fd2eafdf9199f20275d5eb1ee5f7d8b7cb8d
electric-otter made Unxus! <electric-otter-github@proton.me>
2025-06-25 17:33:09 -0400

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -2,12 +2,29 @@ BITS 16
 org 0x7c00
 
 start:
-    ; Print a character (optional)
-    mov ah, 0x0e
-    mov al, 'Boot now'
+    mov ah, 0x0e         ; teletype output
+    mov al, '!'          
     int 0x10
 
-    ; Infinite loop to halt
+    ; Load sector 2 (LBA 1) from disk into 0x7e00
+    mov ah, 0x02         ; BIOS read sectors
+    mov al, 1            ; number of sectors
+    mov ch, 0            ; cylinder
+    mov cl, 2            ; sector (starts at 1)
+    mov dh, 0            ; head
+    mov dl, 0x80         ; first hard disk
+    mov bx, 0x7e00       ; ES:BX buffer (ES=0x0000)
+    int 0x13             ; BIOS disk service
+
+    jc disk_error        ; if carry set, error
+
+    jmp 0x0000:0x7e00    ; jump to loaded code
+
+disk_error:
+    mov al, 'E'
+    int 0x10
+    jmp $
+
 hang:
     jmp hang
 
